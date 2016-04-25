146589dece7d8d3fca25e146570507552c3570ef
Mahmoud <mtohmaz@ncsu.edu>
2016-04-25 15:08:58 -0400

    Fixed issues with reorderlist

--- app/src/main/java/gopackdev/arrivalpack/ReOrderList/ReOrderList.java
+++ app/src/main/java/gopackdev/arrivalpack/ReOrderList/ReOrderList.java
@@ -1,8 +1,6 @@
 package gopackdev.arrivalpack.ReOrderList;
 
-import java.net.MalformedURLException;
 import java.util.ArrayList;
-import java.util.GregorianCalendar;
 import java.util.List;
 
 import gopackdev.arrivalpack.baseactivities.DrawerBaseActivity;
@@ -18,60 +16,72 @@ import gopackdev.arrivalpack.listviewadapter.RoommateRecycleAdapter;
 
 public class ReOrderList extends DrawerBaseActivity {
 
-    private List<StudentBean> student_recom_list = new ArrayList<StudentBean>();
+    boolean gender = false;
+    boolean language = false;
+    boolean nationality = false;
+    boolean dietary_restriction = false;
+    boolean sleepTime = false;
+    boolean wakeTime = false;
 
-//    public ArrayList findMatch(List<StudentBean> student_list, boolean gender, boolean langauge,
-//                          boolean nationality, boolean dietary_restriction,
-//                          boolean sleepTime, boolean wakeTime) throws Exception {
-//        List<StudentBean> reorderedList = new ArrayList<StudentBean>();
-//
-//
-//        for (int i = 0; i < student_list.size(); i++) {
-//            int value = getValues(student_list.get(i), gender, langauge,
-//            nationality, dietary_restriction,
-//            sleepTime, wakeTime);
-//            if (reorderedList.size() == 0) {
-//                reorderedList.add(student_list.get(i));
-//            }
-//            for (int j = 0; j < reorderedList.size(); j++) {
-//                int newValue = getValues(reorderedList.get(j), boolean gender, boolean langauge,
-//                boolean nationality, boolean dietary_restriction,
-//                boolean sleepTime, boolean wakeTime);
-//                if (value >= newValue) {
-//                    reorderedList.add(j, student_list.get(i));
-//                }
-//
-//            }
-//        }
-//        return reorderedList;
-//    }
-//
-//    private int getValues(StudentBean student) {
-//        int genderValue = 0;
-//        int langaugeValue = 0;
-//        int nationalityValue = 0;
-//        int dietaryValue = 0;
-//        int sleepTimeValue = 0;
-//        int wakeTimeValue = 0;
-//
-//        if (gender && super.currentUser.getGender() == student.getGender())
-//            genderValue = 100;
-//
-//        if (language && super.currentUser.getLanguage() == student.getLanguage())
-//            languageValue = 100;
-//
-//        if (nationality && super.currentUser.getNationality() == student.getNationality())
-//            nationality = 100;
-//
-//        if (dietary_restriction && super.currentUser.getDietary_Restriction() == student.getDietary_Restriction())
-//            dietaryValue = 100;
-//
-//        if (sleepTime && super.currentUser.getSleepTime() == student.getSleepTime())
-//            sleepTimeValue = 100;
-//
-//        if (wakeTime && super.currentUser..getWakeTime() == student.getWakeTime())
-//            wakeTimeValue = 100;
-//
-//        return genderValue + langaugeValue + nationality + dietaryValue + sleepTimeValue + wakeTimeValue;
-//    }
+    public List<StudentBean> findMatch(List<StudentBean> student_list, boolean gender,
+                                       boolean language, boolean nationality, boolean
+                                       dietary_restriction, boolean sleepTime,
+                                       boolean wakeTime) throws Exception {
+        this.gender = gender;
+        this.language = language;
+        this.nationality = nationality;
+        this.dietary_restriction = dietary_restriction;
+        this.sleepTime = sleepTime;
+        this.wakeTime = wakeTime;
+
+        List<StudentBean> reorderedList = new ArrayList<StudentBean>();
+
+        for (int i = 0; i < student_list.size(); i++) {
+            int value = getValues(student_list.get(i));
+
+            if (reorderedList.size() == 0) {
+                reorderedList.add(student_list.get(i));
+                continue;
+            }
+            int listsize = reorderedList.size();
+            for (int j = 0; j < listsize ; j++) {
+                int newValue = getValues(reorderedList.get(j));
+                if (value >= newValue) {
+                    reorderedList.add(j, student_list.get(i));
+                    break;
+                }
+                reorderedList.add(student_list.get(i));
+            }
+        }
+        return reorderedList;
+    }
+
+    private int getValues(StudentBean student) {
+        int genderValue = 0;
+        int languageValue = 0;
+        int nationalityValue = 0;
+        int dietaryValue = 0;
+        int sleepTimeValue = 0;
+        int wakeTimeValue = 0;
+
+        if (gender && currentUser.getGender() == student.getGender())
+            genderValue = 100;
+
+        if (language && currentUser.getLanguage() == student.getFirstLanguage())
+            languageValue = 100;
+
+        if (nationality && currentUser.getNationality() == student.getNationality())
+            nationalityValue = 100;
+
+        if (dietary_restriction && currentUser.getDietary_Restriction() == student.getDietaryRestriction())
+            dietaryValue = 100;
+
+        if (sleepTime)
+            sleepTimeValue = Math.abs(currentUser.getSleepTime() - student.getSleepTime());
+
+        if (wakeTime)
+            wakeTimeValue = Math.abs(currentUser.getWakeTime() - student.getWakeupTime());
+
+        return genderValue + languageValue + nationalityValue + dietaryValue + sleepTimeValue + wakeTimeValue;
+    }
 }
