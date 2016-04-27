6406e0629bd00de8fcd1489c03b88dcb2e259fe5
Chi-Han Wang <cwang25@ncsu.edu>
2016-04-27 00:43:50 -0400

    icon changes and improvement on home page

--- app/src/main/java/gopackdev/arrivalpack/MainHomeActivity.java
+++ app/src/main/java/gopackdev/arrivalpack/MainHomeActivity.java
@@ -1,6 +1,7 @@
 package gopackdev.arrivalpack;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import android.os.Handler;
@@ -11,7 +12,12 @@ import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
 
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 
 import com.ibm.mobilefirstplatform.clientsdk.android.core.api.BMSClient;
@@ -39,10 +45,11 @@ public class MainHomeActivity extends DrawerBaseActivity {
     private RecyclerView.LayoutManager mLayoutManager;
     private BMSClient client;
     private List<StudentBean> student_recom_list = new ArrayList<StudentBean>();
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setTitle("Roommate Suggestions");
         FrameLayout relativeLayout = (FrameLayout) findViewById(R.id.child_layout);
         LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         relativeLayout.addView(layoutInflater.inflate(R.layout.content_home_page, null, false));
@@ -69,6 +76,7 @@ public class MainHomeActivity extends DrawerBaseActivity {
         // specify an adapter (see also next example)
         mAdapter = new RoommateRecycleAdapter(student_recom_list, this);
         mRecyclerView.setAdapter(mAdapter);
+        progressBar = (ProgressBar)findViewById(R.id.loading_progress);
         new Handler(Looper.getMainLooper()).post(new Runnable() {
             @Override
             public void run() {
@@ -82,40 +90,87 @@ public class MainHomeActivity extends DrawerBaseActivity {
     }
 
     private void getStudentListData() throws Exception {
-        StudentConnector sc = new StudentConnector(client);
-        GregorianCalendar startTime  = new GregorianCalendar();
-        while(super.currentUser == null){
-            Log.i("busy wait in other thread", "waiting...");
-            GregorianCalendar anchor = new GregorianCalendar();
-            if(startTime.getTimeInMillis() - anchor.getTimeInMillis() > 5000){
-                throw new Exception("Seomthing weird happen to server");
-            }
-        }
-        sc.getStudentsBySchool(this, super.currentUser.getSchoolID(), new ResponseListener() {
+        new AsyncTask<Void, Void, Void>() {
             @Override
-            public void onSuccess(Response response) {
-                try {
-                    JSONArray student_list = new JSONArray(response.getResponseText());
-                    for (int i=0; i<student_list.length(); i++) {
-                        JSONObject tmp_json = student_list.getJSONObject(i);
-                        StudentBean tmp_b = new StudentBean(tmp_json);
-                        student_recom_list.add(tmp_b);
-                    }
-                    new Handler(Looper.getMainLooper()).post(new Runnable() {
-                        @Override
-                        public void run() {
-                            mAdapter.notifyDataSetChanged();
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                StudentConnector sc = new StudentConnector(client);
+                sc.getStudentsBySchool(getApplicationContext(), currentUser.getSchoolID(), new ResponseListener() {
+                    @Override
+                    public void onSuccess(Response response) {
+                        try {
+                            JSONArray student_list = new JSONArray(response.getResponseText());
+                            for (int i=0; i<student_list.length(); i++) {
+                                JSONObject tmp_json = student_list.getJSONObject(i);
+                                StudentBean tmp_b = new StudentBean(tmp_json);
+                                student_recom_list.add(tmp_b);
+                            }
+                            new Handler(Looper.getMainLooper()).post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mAdapter.notifyDataSetChanged();
+                                }
+                            });
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
-                    });
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+                    }
+
+                    @Override
+                    public void onFailure(Response response, Throwable t, JSONObject extendedInfo) {
+                        Log.i("get student list by school","Error: "+response);
+                    }
+                });
+                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
             }
 
             @Override
-            public void onFailure(Response response, Throwable t, JSONObject extendedInfo) {
-                Log.i("get student list by school","Error: "+response);
+            protected Void doInBackground(Void... params) {
+                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBar.setVisibility(View.VISIBLE);
+                    }
+                });
+                while(currentUser == null){
+                    GregorianCalendar startTime  = new GregorianCalendar();
+                    Log.i("busy wait in other thread", "waiting...");
+                    GregorianCalendar anchor = new GregorianCalendar();
+                    if(startTime.getTimeInMillis() - anchor.getTimeInMillis() > 5000){
+                        Log.i("server not responding", "error server not responding");
+                    }
+                }
+
+                return null;
             }
-        });
+        }.execute();
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_home, menu);
+        return true;
+    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle item selection
+//        switch (item.getItemId()) {
+//            case R.id.new_game:
+//                newGame();
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
+    public void openSortBySetup(MenuItem item){
+        Toast.makeText(this, "Sort By clicked!",Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/gopackdev/arrivalpack/baseactivities/DrawerBaseActivity.java
+++ app/src/main/java/gopackdev/arrivalpack/baseactivities/DrawerBaseActivity.java
@@ -237,12 +237,12 @@ public abstract class DrawerBaseActivity extends AppCompatActivity
         }*/
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_home, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main_home, menu);
+//        return true;
+//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
